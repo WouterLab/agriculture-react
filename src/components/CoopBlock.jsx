428c923aa218c71be8428b0cf5c773d16b4f4a12
WouterLab <panov135@mail.ru>
2022-11-30 12:33:27 +0300

    coop page

---
+++ src/components/CoopBlock.jsx
@@ -0,0 +1,16 @@
+import { BriefcaseIcon } from "@heroicons/react/24/outline";
+
+const CoopBlock = ({ job, desc }) => {
+  return (
+    <div className='bg-gray-100 mb-4 px-2 py-4'>
+      <div className='flex items-center'>
+        <BriefcaseIcon className='w-12' />
+        <p className='font-bold text-2xl'>{job}</p>
+      </div>
+      <p className='mb-4'>{desc}</p>
+      <button className='rounded-2xl px-4 py-2 font-semibold bg-green-400 hover:text-white transition-all duration-300'>Связаться</button>
+    </div>
+  );
+};
+
+export default CoopBlock;
--- src/pages/CoopPage.jsx
+++ src/pages/CoopPage.jsx
@@ -1,16 +1,54 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import CoopBlock from "../components/CoopBlock";
 import Layout from "../components/Layout";
 import PageTitle from "../components/PageTitle";
 
 const CoopPage = () => {
+  const persons = [
+    {
+      id: 0,
+      job: "Фермерство и уход за плантациями",
+      desc: "Описание вакансии для сотрудничества",
+    },
+    {
+      id: 2,
+      job: "Поставщик молочной продукции",
+      desc: "Описание вакансии для сотрудничества",
+    },
+    {
+      id: 3,
+      job: "Поставщик кукурузы",
+      desc: "Описание вакансии для сотрудничества",
+    },
+    {
+      id: 4,
+      job: "Продуктовый менеджер",
+      desc: "Описание вакансии для сотрудничества",
+    },
+    {
+      id: 5,
+      job: "Вакансия для сотрудничества",
+      desc: "Описание вакансии для сотрудничества",
+    },
+  ];
   return (
     <Layout>
       <div className='flex flex-col items-center'>
-        <div className='flex items-center space-x-4'>
+        <div className='flex items-center space-x-4 mb-8'>
           <Link to='/vacancy'>
-            <h2 className='text-4xl font-bold text-neutral-500 underline'>ВАКАНСИИ</h2>
+            <h2 className='text-4xl font-bold text-neutral-500 underline'>
+              ВАКАНСИИ
+            </h2>
           </Link>
           <PageTitle title='СОТРУДНИЧЕСТВО' />
+        </div>
+        <div className='flex justify-between w-4/5'>
+          <aside className='w-1/5'>sidebar</aside>
+          <div className='w-4/5'>
+            {persons.map((person) => (
+              <CoopBlock key={person.id} job={person.job} desc={person.desc} />
+            ))}
+          </div>
         </div>
       </div>
     </Layout>
